ddb59e52bea622ba5def21fcaded14f80e4e156f
V3ckt0r <burhan_abdi@hotmail.co.uk>
2017-01-27 11:48:37 +0000

    refactored response check, changed argument flags, included inspect, changed main function logic.

--- quickspin.py
+++ quickspin.py
@@ -3,16 +3,19 @@
 import boto3
 import argparse
 import sys
+import inspect
 
 ec2 = boto3.resource('ec2')
 client = boto3.client('ec2')
 
 # Set up acceptable arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-s","--start", nargs='+', help="List of EC2 ids", required=False)
+parser.add_argument("-u","--up", nargs='+', help="List of EC2 ids to bring up", required=False)
+parser.add_argument("-d","--down", nargs='+', help="List of EC2 ids to bring down", required=False)
 parser.add_argument("-l", "--list", help="show all EC2 instances running", action="store_true")
 parser.add_argument("-v", "--verbose", help="show api call to googleapi", action="store_true")
 args = parser.parse_args()
+
 
 # List running instances
 def listRunning():
@@ -26,9 +29,23 @@ def listRunning():
 def upIt(instance_list):
 
     response = client.start_instances( InstanceIds=instance_list, AdditionalInfo='string', DryRun=False)
+    responseCheck(response)
 
-    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+def downIt(instance_list):
+
+    response = client.stop_instances( InstanceIds=instance_list, Force=False, DryRun=False)
+    responseCheck(response)
+
+def responseCheck(response):
+    curframe = inspect.currentframe()
+    calframe = inspect.getouterframes(curframe, 2)
+    callingFrame = calframe[1][3]
+
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200 and callingFrame == "upIt":
         print "Instance have all started sucessfully..."
+        return 0
+    elif response['ResponseMetadata']['HTTPStatusCode'] == 200 and callingFrame == "downIt":
+        print "Instance have all been stopped sucessfully..."
         return 0
     else:
         error_reponse = response['ResponseMetadata']['HTTPStatusCode']
@@ -39,15 +56,22 @@ def runIt():
     pass
 
 def main():
-    if args.start == None:
+    if len(sys.argv) <= 1:
         print "You must use a flag to tell quickspin what to do... use -h for help"
         sys.exit(1)
 
-    if args.list == True:
+    if args.list:
         listRunning()
         sys.exit(0)
 
-    upIt(args.start)
+    if args.up:
+        upIt(args.up)
+        sys.exit(0)
+
+    if args.down:
+        downIt(args.down)
+        sys.exit(0)
+
     print "thats it"
     sys.exit(0)
 
