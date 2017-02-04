b08f7ee8e3f66905eed2f8d09d13a44440428846
V3ckt0r <burhan_abdi@hotmail.co.uk>
2017-02-04 19:55:27 +0000

    Added build files

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-####Quickspin
+##Quickspin
 
 Quickspin is a simple CLI tool for spinning up a series of EC2 instances.
+
+###How to install
+You can install Quickspin using **pip**
+    pip install Quickspin
+
+source install via:
+    python setup.py install
+
+####Quick Start
+Once installed use you can get help on use Quickspin as follows:
+    quickspin -h
---
+++ setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="Quickspin",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="V3ckt0r",
+    author_email="",
+
+    # Packages
+    packages=["Quickspin"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    entry_points={
+            'console_scripts': [
+                'quickspin = quickspin:main',
+            ]
+    ]
+                                                                            }
+
+    # Details
+    url="https://github.com/V3ckt0r/Quickspin",
+
+    #
+    license="LICENSE.txt",
+    description="Quickspin CLI tool for managing AWS resources",
+
+    long_description=open("README.md").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "boto3",
+    ],
+)
